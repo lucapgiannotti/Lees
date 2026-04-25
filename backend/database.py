4aa87c64fdc195ed047cf96d23abb8a7c1f0405d
Luca Giannotti <dev@lucagiannotti.com>
2026-04-25 16:16:42 -0500

    formatting

--- backend/database.py
+++ backend/database.py
@@ -1,20 +1,19 @@
 # backend/database.py
 import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Check if we are running inside a Docker container
-IS_DOCKER = os.path.exists('/.dockerenv')
+IS_DOCKER = os.path.exists("/.dockerenv")
 
 if IS_DOCKER:
     SQLALCHEMY_DATABASE_URL = "sqlite:////app/data/lees_tracker.db"
 else:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./lees_tracker.db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
